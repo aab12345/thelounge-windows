91514fc32cb21d5a330b96978dccb40032d1d652
aab12345 <5588454+aab12345@users.noreply.github.com>
2021-10-18 18:21:35 +0100

    Delete TLC4.2.0/.Shortcuts directory

--- TLC4.2.0/.Shortcuts/Private.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-TITLE The Lounge Chat 4.2.0 PRIVATE MODE SERVER RUNNING
-thelounge start THELOUNGE_HOME=/.thelounge
--- TLC4.2.0/.Shortcuts/Public.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-TITLE The Lounge Chat 4.2.0 PUBLIC MODE SERVER RUNNING
-thelounge start -c port=9001 -c public=true
